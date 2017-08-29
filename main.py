480436637c50edaf115cde49d79bcabfbf2f448f
lschirmer <schirmer.luizj@gmail.com>
2017-08-29 19:06:24 -0300

    selection

--- main.py
+++ main.py
@@ -17,11 +17,13 @@ if __name__ == '__main__':
     # cf.jaccard = 1.0
     weights = [1.0,0.5,0.0,0.5,1.0,0.0]
     #sample size
-    size = 100
+    size = 300
 
     #params = size of sample set and list of weight
     data = dataprovider.DataProvider(size, weights)
     endsit = data.GetEndSituation()
     dots = data.Calculate()
+    mds = mds.mdsclass()
     pos = mds.mdsGen(dots,endsit)
     mds.drawPlot(size,pos,endsit)
+
--- mds.py
+++ mds.py
@@ -8,65 +8,89 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 import numpy as np
 
-def mdsGen(data, weights):
-    print("initializing MDS")
-    mds = manifold.MDS(n_components=3, metric=True, n_init=4, max_iter=100, n_jobs=8, dissimilarity="precomputed")
-    pos = mds.fit(data).embedding_
-    print("creating plot")
 
-    #cl = np.zeros((pos.shape[0], 1))
-    #for i in range(100):
-    #    cl[i] = weights[i]
-    #print(pos.shape, cl.shape)
-    #pos = np.hstack((pos, cl))
-    print(pos.shape)
-    return pos
+class mdsclass(object):
+    def __init__(self):
+        self.clicked = False
+        self.pos = []
 
-def drawPlot(size,pos,endsit):
-    red = 0
-    green = 0
-    blue = 0
+    def mdsGen(self, data, weights):
+        print("initializing MDS")
+        mds = manifold.MDS(n_components=3, metric=True, n_init=4, max_iter=100, n_jobs=8, dissimilarity="precomputed")
+        self.pos = mds.fit(data).embedding_
+        print("stress")
+        #sum = (self.pos**2).sum
+        #print(np.sqrt(mds.stress_() / sum))
+        print("creating plot")
 
-    for i in range(size):
-        if (endsit[i] == 0):
-            green = green + 1
-        elif (endsit[i] == 1):
-            red = red + 1
-        else:
-            blue = blue + 1
+        #cl = np.zeros((pos.shape[0], 1))
+        #for i in range(100):
+        #    cl[i] = weights[i]
+        #print(pos.shape, cl.shape)
+        #pos = np.hstack((pos, cl))
+        print(self.pos.shape)
+        return self.pos
 
-    aproved = np.zeros((green, 3))
-    denied = np.zeros((red, 3))
-    canceled = np.zeros((blue, 3))
 
-    k1 = 0
-    k2 = 0
-    k3 = 0
-    for i in range(size):
-        if (endsit[i] == 0):
-            aproved[k1, :] = pos[i, :]
-            k1 = k1 + 1
-        elif (endsit[i] == 1):
-            denied[k2, :] = pos[i, :]
-            k2 = k2 + 1
-        else:
-            canceled[k3, :] = pos[i, :]
-            k3 = k3 + 1
+    def onHover(self,event):
+        if self.clicked == True:
+            print('x=%d, y=%d ' %
+                 (event.x, event.y))
 
-    fig, ax = plt.subplots()
-    color = ['green', 'red', 'blue']
-    labels = ['Aproved', 'Denied', 'Canceled']
-    ax = Axes3D(fig)
-    ax.scatter(aproved[:, 0], aproved[:, 1], aproved[:, 2], c='green',
-               alpha=0.7, edgecolors='none')
-    ax.scatter(denied[:, 0], denied[:, 1], denied[:, 2], c='red',
-               alpha=0.7, edgecolors='none')
-    ax.scatter(canceled[:, 0], canceled[:, 1], canceled[:, 2], c='blue',
-               alpha=0.7, edgecolors='none')
+    def onclick(self,event):
 
-    green_patch = mpatches.Patch(color='green', label='Aproved')
-    red_patch = mpatches.Patch(color='red', label='Denied')
-    blue_patch = mpatches.Patch(color='blue', label='Canceled')
-    ax.legend(handles=[green_patch, red_patch, blue_patch])
+        if event.button == 2:
+            self.clicked = True
 
-    plt.show()
+
+
+    def drawPlot(self, size,pos,endsit):
+        red = 0
+        green = 0
+        blue = 0
+
+        for i in range(size):
+            if (endsit[i] == 0):
+                green = green + 1
+            elif (endsit[i] == 1):
+                red = red + 1
+            else:
+                blue = blue + 1
+
+        aproved = np.zeros((green, 3))
+        denied = np.zeros((red, 3))
+        canceled = np.zeros((blue, 3))
+
+        k1 = 0
+        k2 = 0
+        k3 = 0
+        for i in range(size):
+            if (endsit[i] == 0):
+                aproved[k1, :] = pos[i, :]
+                k1 = k1 + 1
+            elif (endsit[i] == 1):
+                denied[k2, :] = pos[i, :]
+                k2 = k2 + 1
+            else:
+                canceled[k3, :] = pos[i, :]
+                k3 = k3 + 1
+
+        fig, ax = plt.subplots()
+        color = ['green', 'red', 'blue']
+        labels = ['Aproved', 'Denied', 'Canceled']
+        ax = Axes3D(fig)
+        ax.scatter(aproved[:, 0], aproved[:, 1], aproved[:, 2], c='green',
+                   alpha=0.7, edgecolors='none')
+        ax.scatter(denied[:, 0], denied[:, 1], denied[:, 2], c='red',
+                   alpha=0.7, edgecolors='none')
+        ax.scatter(canceled[:, 0], canceled[:, 1], canceled[:, 2], c='blue',
+                   alpha=0.7, edgecolors='none')
+
+        green_patch = mpatches.Patch(color='green', label='Aproved')
+        red_patch = mpatches.Patch(color='red', label='Denied')
+        blue_patch = mpatches.Patch(color='blue', label='Canceled')
+        ax.legend(handles=[green_patch, red_patch, blue_patch])
+
+        fig.canvas.mpl_connect('button_press_event', self.onclick)
+        fig.canvas.mpl_connect('motion_notify_event', self.onHover)
+        plt.show()
