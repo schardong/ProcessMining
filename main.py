885566f3dce5c11875d525a9b0a7c78e5ecc0338
lschirmer <schirmer.luizj@gmail.com>
2017-08-30 19:06:32 -0300

    selection add and 2d

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # cf.jaccard = 1.0
     weights = [1.0,0.5,0.0,0.5,1.0,0.0]
     #sample size
-    size = 300
+    size = 2000
 
     #params = size of sample set and list of weight
     data = dataprovider.DataProvider(size, weights)
--- mds.py
+++ mds.py
@@ -1,11 +1,10 @@
 # MDS function and distance dissimilarity
-
-
-
 from sklearn import manifold
 import matplotlib.patches as mpatches
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
 from matplotlib import pyplot as plt
+import copy
 import numpy as np
 
 
@@ -13,84 +12,65 @@ class mdsclass(object):
     def __init__(self):
         self.clicked = False
         self.pos = []
+        self.selected = []
 
     def mdsGen(self, data, weights):
         print("initializing MDS")
-        mds = manifold.MDS(n_components=3, metric=True, n_init=4, max_iter=100, n_jobs=8, dissimilarity="precomputed")
+        mds = manifold.MDS(n_components=2, metric=True, n_init=4, max_iter=100, n_jobs=8, dissimilarity="precomputed")
         self.pos = mds.fit(data).embedding_
-        print("stress")
-        #sum = (self.pos**2).sum
-        #print(np.sqrt(mds.stress_() / sum))
-        print("creating plot")
-
-        #cl = np.zeros((pos.shape[0], 1))
-        #for i in range(100):
-        #    cl[i] = weights[i]
-        #print(pos.shape, cl.shape)
-        #pos = np.hstack((pos, cl))
-        print(self.pos.shape)
         return self.pos
 
+    def mdsStress(self):
+        return 0
 
-    def onHover(self,event):
+    def distance(self, point, event):
+        assert point.shape == (2,), "distance: point.shape is wrong: %s, must be (3,)" % point.shape
+        # Convert 2d data space to 2d screen space
+        x3, y3 = self.ax.transData.transform((point[0], point[1]))
+        return np.sqrt((x3 - event.x) ** 2 + (y3 - event.y) ** 2)
+
+    def calcClosestDatapoint(self, event):
+        """"Calculate which data point is closest to the mouse position.
+        Args:
+            X (np.array) - array of points, of shape (numPoints, 3)
+            event (MouseEvent) - mouse event (containing mouse position)
+        Returns:
+            smallestIndex (int) - the index (into the array of points X) of the element closest to the mouse position
+        """
+        distances = np.array([self.distance(self.pos[i, 0:2], event) for i in range(self.pos.shape[0])])
+        brushlist = np.where(np.logical_and(distances >= 0, distances <= 10))
+        return brushlist[0]
+
+    def onHover(self, event):
         if self.clicked == True:
-            print('x=%d, y=%d ' %
-                 (event.x, event.y))
+            d = self.calcClosestDatapoint(event)
+            self.selected.extend(d)
+            self.selected = list(set(self.selected))
+            for i in range(self.selected.__len__()):
+                self.coll._facecolors[self.selected[i], :] = (1, 0.54, 0, 1)
+            self.fig.canvas.draw()
 
-    def onclick(self,event):
+    def buttonClick(self, event):
+        self.coll._facecolors[event.ind, :] = (1, 0.54, 0, 1)
+        self.fig.canvas.draw()
+        self.clicked = True
 
-        if event.button == 2:
-            self.clicked = True
 
+    def buttonRelease(self, event):
+        if event.button == 1:
+            self.clicked = False
 
-
-    def drawPlot(self, size,pos,endsit):
-        red = 0
-        green = 0
-        blue = 0
-
-        for i in range(size):
-            if (endsit[i] == 0):
-                green = green + 1
-            elif (endsit[i] == 1):
-                red = red + 1
-            else:
-                blue = blue + 1
-
-        aproved = np.zeros((green, 3))
-        denied = np.zeros((red, 3))
-        canceled = np.zeros((blue, 3))
-
-        k1 = 0
-        k2 = 0
-        k3 = 0
-        for i in range(size):
-            if (endsit[i] == 0):
-                aproved[k1, :] = pos[i, :]
-                k1 = k1 + 1
-            elif (endsit[i] == 1):
-                denied[k2, :] = pos[i, :]
-                k2 = k2 + 1
-            else:
-                canceled[k3, :] = pos[i, :]
-                k3 = k3 + 1
-
-        fig, ax = plt.subplots()
-        color = ['green', 'red', 'blue']
+    def drawPlot(self, size, pos, endsit):
+        self.fig, self.ax = plt.subplots()
         labels = ['Aproved', 'Denied', 'Canceled']
-        ax = Axes3D(fig)
-        ax.scatter(aproved[:, 0], aproved[:, 1], aproved[:, 2], c='green',
-                   alpha=0.7, edgecolors='none')
-        ax.scatter(denied[:, 0], denied[:, 1], denied[:, 2], c='red',
-                   alpha=0.7, edgecolors='none')
-        ax.scatter(canceled[:, 0], canceled[:, 1], canceled[:, 2], c='blue',
-                   alpha=0.7, edgecolors='none')
-
-        green_patch = mpatches.Patch(color='green', label='Aproved')
+        self.coll = self.ax.scatter(pos[:, 0], pos[:, 1], c=endsit, cmap='brg', picker=5, alpha=0.7,
+                                      edgecolors='none')
+        plt.axis([-1, 1, -1, 1])
+        green_patch = mpatches.Patch(color='blue', label='Aproved')
         red_patch = mpatches.Patch(color='red', label='Denied')
-        blue_patch = mpatches.Patch(color='blue', label='Canceled')
-        ax.legend(handles=[green_patch, red_patch, blue_patch])
-
-        fig.canvas.mpl_connect('button_press_event', self.onclick)
-        fig.canvas.mpl_connect('motion_notify_event', self.onHover)
+        blue_patch = mpatches.Patch(color='lawngreen', label='Canceled')
+        self.ax.legend(handles=[green_patch, red_patch, blue_patch])
+        self.fig.canvas.mpl_connect('pick_event', self.buttonClick)
+        self.fig.canvas.mpl_connect('motion_notify_event', self.onHover)
+        self.fig.canvas.mpl_connect('button_release_event', self.buttonRelease)
         plt.show()
